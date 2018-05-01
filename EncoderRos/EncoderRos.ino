bc3e7c56a1540f3982e796d81fc02633cd398e97
Emiliano Borghi <emiliano_borghi@hotmail.com>
2018-05-01 16:29:28 -0300

    Add EncoderRos example

---
+++ EncoderRos/EncoderRos.ino
@@ -0,0 +1,35 @@
+#include <ros.h>
+#include <std_msgs/UInt16.h>
+
+const int PIN = A2;
+
+ros::NodeHandle  nh;
+std_msgs::UInt16 msg;
+ros::Publisher pub("encoder", &msg);
+
+uint16_t oldState;
+uint16_t newState;
+
+void setup() {
+  pinMode(PIN, INPUT);
+  nh.initNode();
+  nh.advertise(pub);
+  oldState = digitalRead(PIN);
+}
+
+void loop() {
+  // Envia cada flanco creciente
+  newState = digitalRead(PIN);
+  if(oldState != newState)
+  {
+    if(newState)
+    {
+      // Agregar timestamp
+      msg.data = newState;
+      pub.publish( &msg );
+    }
+    oldState = newState;
+  }
+  nh.spinOnce();
+  delay(200);
+}
