93ec2cacc8a3567c7365fc56d9e8cd965e8eb7d9
Emiliano Borghi <eborghi@ekumenlabs.com>
2018-05-10 21:28:37 -0300

    Add odometry using rosabridge package

---
+++ MyRobotRos/README.md
@@ -0,0 +1,7 @@
+# My ROS robot
+
+`roscore`
+
+`rosrun rosserial_python serial_node.py _port:=/dev/ttyACM0`
+
+`roslaunch rosabridge_server odom_proxy_node.launch`
--- MyRobotRos/encoder.hpp
+++ MyRobotRos/encoder.hpp
@@ -9,6 +9,8 @@
 ros::Time last_time;
 ros::Time current_time;
 
+double dT;
+
 std_msgs::Float32 encoder_left_msg;
 std_msgs::Float32 encoder_right_msg;
 
@@ -30,7 +32,11 @@ double wr;
 
 double read2angle(uint16_t register_output) 
 {
-  return register_output * (static_cast<double>(TWO_PI / 16383.0));
+  double angle = register_output * (static_cast<double>(TWO_PI / 16383.0));
+
+  // |angle| < DEVICE_RESOLUTION
+  if(abs(angle) < 0.0001745329) return 0.0;
+  return angle;
 }
 
 void ReadAngles() 
@@ -49,8 +55,8 @@ void ReadAngles()
 
 double get_velocity(Angle angle_t1, Angle angle_t0, ros::Time t1, ros::Time t0){
   Angle dAngle(angle_t1 - angle_t0);
-  double dT = (t1.toNsec() - t0.toNsec())/1E9;
-  return dAngle.GetAngle();// / dT;
+  dT = (t1.toSec() - t0.toSec()) + (t1.toNsec() - t0.toNsec())/1E9;
+  return dAngle.GetAngle() / dT;
 }
 
 
@@ -62,14 +68,14 @@ void encodersLogic(){
   wl = get_velocity(
       current_left_motor_angle, last_left_motor_angle,
       current_time, last_time);
-  encoder_left_msg.data = wl;
-  encoder_left_pub.publish( &encoder_left_msg );
+//  encoder_left_msg.data = wl;
+//  encoder_left_pub.publish( &encoder_left_msg );
 
   wr = (-1) * get_velocity(
         current_right_motor_angle, last_right_motor_angle,
         current_time, last_time);
-  encoder_right_msg.data = wr;
-  encoder_right_pub.publish( &encoder_right_msg );
+//  encoder_right_msg.data = wr;
+//  encoder_right_pub.publish( &encoder_right_msg );
 
   last_right_motor_angle = current_right_motor_angle;
   last_left_motor_angle = current_left_motor_angle;
