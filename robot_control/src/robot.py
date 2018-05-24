78305f12968c8e8bf42bd3f959a28cf5a95b4064
Emiliano Borghi <eborghi@ekumenlabs.com>
2018-05-24 20:44:57 -0300

    Add first controller approach

--- robot_control/src/robot.py
+++ robot_control/src/robot.py
@@ -1,29 +1,41 @@
 #!/usr/bin/env python
 
 import rospy
-from std_msgs.msg import Float32, UInt16
+from std_msgs.msg import Float32, Int16
 
-ping_ = 100
+def encoder_left_cb(data): # rad/s
+    global encoder_left_msg
+    encoder_left_msg = data.data * 10
 
-def callback(data):
-    global ping_
-    ping_ = data.data
+def encoder_right_cb(data):
+    global encoder_right_msg
+    encoder_right_msg = data.data * 10
 
-def listener():
+def main():
+    rospy.init_node('robot_controller', anonymous=True)
 
-    rospy.init_node('ping_listener', anonymous=True)
-    rospy.Subscriber("ping", Float32, callback)
-    left_motor_pub = rospy.Publisher('output/left', UInt16, queue_size=1)
-    right_motor_pub = rospy.Publisher('output/right', UInt16, queue_size=1)
-    rate = rospy.Rate(4)
+    rospy.Subscriber("encoder/left", Float32, encoder_left_cb)
+    rospy.Subscriber("encoder/right", Float32, encoder_right_cb)
+    left_motor_pub = rospy.Publisher('output/left', Int16, queue_size=1)
+    right_motor_pub = rospy.Publisher('output/right', Int16, queue_size=1)
+
+    rate = rospy.Rate(100) # Hz
+
+    # Publish final Velocities
+    left_motor_pub.publish(200)
+    right_motor_pub.publish(200)
+
+    global encoder_left_msg
+    global encoder_right_msg
+    encoder_left_msg = 0
+    encoder_right_msg = 0
+
     while not rospy.is_shutdown():
-        if ping_ > 0.5:
-            right_motor_pub.publish(100)
-            left_motor_pub.publish(100)
-        else:
-            right_motor_pub.publish(0)
-            left_motor_pub.publish(0)
+        error_left = 200 - encoder_left_msg
+        left_motor_pub.publish(200 + error_left)
+        error_right = 200 - encoder_right_msg
+        right_motor_pub.publish(200 + error_right)
         rate.sleep()
 
 if __name__ == '__main__':
-    listener()
+    main()
