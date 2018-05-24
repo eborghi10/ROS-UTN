a08964dc0bccef98a93ba201f7952710059de957
Emiliano Borghi <eborghi@ekumenlabs.com>
2018-05-24 20:23:57 -0300

    Fix sensor velocity

--- MyRobotRos/encoder.hpp
+++ MyRobotRos/encoder.hpp
@@ -29,13 +29,13 @@ ros::Publisher encoder_right_pub("encoder/right", &encoder_right_msg);
 double wl;
 double wr;
 
-Spline<double> spline_left(real_left, ideal, NUM_POINTS);
+Spline<double> spline_left(real_left, ideal_left, NUM_POINTS_LEFT);
+Spline<double> spline_right(real_right, ideal_right, NUM_POINTS_RIGHT);
 
 double get_velocity(Angle angle_t1, Angle angle_t0, ros::Time t1, ros::Time t0){
   // Angle unwrapping
   double dAngle = Angle::Unwrap_PI(angle_t0, angle_t1);
-  // return dAngle / dT;
-  return angle_t1.GetAngle();
+  return dAngle / dT;
 }
 
 void encodersLogic(){
@@ -44,8 +44,7 @@ void encodersLogic(){
   double raw_angle_right = encoder_right.getRotationInRadians();
   // Angle calibration
   current_left_motor_angle = spline_left.value(raw_angle_left);
-  // TODO: Create spline_right taking real_right values of data (angles)
-  current_right_motor_angle = spline_left.value(raw_angle_right);
+  current_right_motor_angle = spline_right.value(raw_angle_right);
 
   wl = (-1) * get_velocity(
       current_left_motor_angle, last_left_motor_angle,
@@ -62,4 +61,3 @@ void encodersLogic(){
   last_right_motor_angle = current_right_motor_angle;
   last_left_motor_angle = current_left_motor_angle;
 }
-
